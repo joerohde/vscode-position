c8fe03775fb133edecd568bf796f5c0059483bfe
Joe Rohde <Joe.Rohde@HBO.com>
2018-02-27 09:09:19 -0800

    Functionality Implementation

--- README.md
+++ README.md
@@ -1,65 +1,27 @@
-# jtr-position README
+# vscode-position README
 
-This is the README for your extension "jtr-position". After writing up a brief description, we recommend including the following sections.
+This is the README "vscode-position".  I had been using the [Positions](https://github.com/Silvenga/vscode-positions) extension for seeing the offset but had a need to set the position by offset easily.  So I wrote this, partially because the mentioned extension to learn a bit about typescript.  As I finished I came across [show-offset](https://github.com/ramya-rao-a/show-offset) which I wish I'd run across initially.  Nevertheless, I had added a couple unique features when I wrote this, so I figured I'd toss it into the gallery.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+- Shows cursor character offset as 'Pos \<position>' in the status bar.
+- Click the status bar or use the `Go To Position` palette command to move the cursor by offset.
+- New position is previewed while typing it in (similar to goto line)
+- Cursor and selection restored if setting position is abandoned with <kbd>esc</kbd>
+- Cursor position can also be <kbd>+</kbd><kbd>*number*</kbd> or <kbd>-</kbd><kbd>*number*</kbd> for moving the cursor relative to the current position.
 
-For example if there is an image subfolder under your extension project workspace:
+> Initial keybinding for `Go To Position`: <kbd>⌃</kbd>+<kbd>Alt</kbd>+<kbd>G</kbd> (Mac:  <kbd>⌃</kbd>+<kbd>⌘</kbd>+<kbd>G</kbd>)
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+![Overview](other/PositionAnim.gif)
 
 ## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
+None
 ## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+None
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
 ## Release Notes
-
-Users appreciate release notes as you update your extension.
-
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Initial release.
---
+++ extension.ts
@@ -0,0 +1,196 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    // This code will only be executed once when your extension is activated
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // console.log('Congratulations, your extension "vscode-position" is now active!');
+
+    var controller = new PositionController();
+    context.subscriptions.push(controller);
+    context.subscriptions.push(vscode.commands.registerCommand('position.goto', () => controller.goToPositionCommand()));
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
+function getOptionalNumber(numberAsString ?: string): [number | undefined, boolean] {
+    let optionalNumber: number | undefined = undefined;
+    let isRelative: boolean = false;
+
+    let num: number = Number(numberAsString);
+    if (!isNaN(num) && numberAsString && numberAsString.length) {
+        optionalNumber = num;
+        isRelative = ['+', '-'].includes(numberAsString!.charAt(0));
+    }
+    return [optionalNumber, isRelative];
+}
+
+class PositionController {
+    private disposable: vscode.Disposable;
+    private statusBarItem: vscode.StatusBarItem;
+
+    constructor() {
+        this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+        this.statusBarItem.command = 'position.goto';
+        this.statusBarItem.tooltip = "Go to Position";
+
+        // subscribe to selection change and editor activation events
+        let subscriptions: vscode.Disposable[] = [];
+        vscode.window.onDidChangeTextEditorSelection(this.onEvent, this, subscriptions);
+        vscode.window.onDidChangeActiveTextEditor(this.onEvent, this, subscriptions);
+
+        // create a combined disposable from both event subscriptions
+        this.disposable = vscode.Disposable.from(...subscriptions);
+        this.updatePosition();
+    }
+
+    public goToPositionCommand(): void {
+         // declaring manager? as optional makes .then/async blocks 'forget' the
+         // definite assignment/null check inference done after Create() in tslint.
+        let manager: CursorManager;
+        manager = CursorManager.Create()!;
+        if (!manager) { return; }
+
+        vscode.window.showInputBox({
+            prompt: `Type an offset number from 0 to ${manager.maxPosition}.`,
+            value: String(manager.cursorOffset),
+            validateInput: (input: string) => {
+                manager.previewCursorOffset(input);
+                return undefined;
+            }
+        }).then((input?: string) => {
+            input !== undefined ? manager.commit() : manager.abort();
+        });
+    }
+
+    private updatePosition(): void {
+        let manager = CursorManager.Create();
+        if (!manager) {
+            this.statusBarItem.hide();
+            return;
+        }
+
+        let offset = manager.cursorOffset;
+
+        if (offset !== undefined) {
+            // Update the status bar
+            this.statusBarItem.text = `Pos ${offset}`;
+            this.statusBarItem.show();
+        }
+    }
+
+    private onEvent(): void {
+        this.updatePosition();
+    }
+
+    public dispose() {
+        this.disposable.dispose();
+        this.statusBarItem.dispose();
+    }
+}
+
+class CursorManager {
+    private static CursorPositionDecoration = vscode.window.createTextEditorDecorationType({
+        borderColor: new vscode.ThemeColor('editor.foreground'),
+        borderStyle: 'solid',
+        borderWidth: '1px',
+        outlineColor: new vscode.ThemeColor('editor.foreground'),
+        outlineStyle: 'solid',
+        outlineWidth: '1px',
+    });
+    // Originally preview was implemented without actually moving the cursor, but that was weird when
+    // the real cursor highlight and the mimicked one were both visible.  Leaving this in just because.
+    // private static CursorLineDecoration = vscode.window.createTextEditorDecorationType({
+    //     backgroundColor: new vscode.ThemeColor('editor.lineHighlightBackground'),
+    //     borderColor: new vscode.ThemeColor('editor.lineHighlightBorder'),
+    //     borderStyle: 'solid',
+    //     borderWidth: '1px',
+    //     isWholeLine: true,
+    // });
+
+    public static Create()
+    {
+        let editor = vscode.window.activeTextEditor;
+        let doc = editor ? editor.document : undefined;
+        if (!doc) { return undefined; }
+
+        return new CursorManager(editor!, doc);
+    }
+
+    private readonly originalCursorOffset: number;
+    private readonly cachedSelections: vscode.Selection[];
+
+    constructor(readonly editor: vscode.TextEditor, readonly document: vscode.TextDocument) {
+        this.originalCursorOffset = document.offsetAt(editor.selection.active);
+        this.cachedSelections = [editor.selection]; // dup active selection for our working copy
+        this.cachedSelections.push(...editor.selections);
+    }
+
+    public get cursor() : vscode.Position {
+        return this.editor.selection.active;
+    }
+    public get cursorOffset() : number {
+         return this.document.offsetAt(this.cursor);
+    }
+    // public get selections() : vscode.Selection[] {
+    //     return this.editor.selections;
+    // }
+    public set selections(selections: vscode.Selection[]) {
+        this.editor.selections = selections;
+    }
+
+    public get maxPosition(): number {
+        return this.document.offsetAt(new vscode.Position(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER));
+    }
+
+    public previewCursorOffset(offset?: string): boolean {
+        let [ newOffset, isRelative ] = getOptionalNumber(offset);
+        let success = false;
+
+        if (newOffset !== undefined) {
+            // Show an outline of where the cursor would be placed, and make it visible.
+            if (isRelative) {
+                newOffset! += this.originalCursorOffset;
+            }
+            if (newOffset !== this.cursorOffset) {
+                let newPosition = this.document.positionAt(newOffset);
+                // this.editor.selection = new vscode.Selection(newPosition, newPosition);
+                this.cachedSelections[0] = new vscode.Selection(newPosition, newPosition);
+                this.editor.selections = this.cachedSelections;
+            }
+            success = true;
+        }
+        const range = new vscode.Range(this.cursor, this.cursor.translate(0, 1));
+        this.editor.setDecorations(CursorManager.CursorPositionDecoration, [range]);
+        this.reveal();
+        return success;
+    }
+
+    public commit() {
+        this.clearDecorations();
+        this.editor.selection = this.cachedSelections[0];
+    }
+
+    public abort() {
+        this.clearDecorations();
+        this.cachedSelections.splice(0,1);
+        this.editor.selections = this.cachedSelections;
+        this.reveal();
+    }
+
+    private clearDecorations(): void {
+        this.editor.setDecorations(CursorManager.CursorPositionDecoration, []);
+    }
+
+    private reveal(revealType?: vscode.TextEditorRevealType): void {
+        revealType = revealType || vscode.TextEditorRevealType.InCenterIfOutsideViewport;
+        this.editor.revealRange(this.editor.selection, revealType);
+    }
+}
